a1ba59053a952cbe2b877b6f6cf1d8859d32fba7
ximeen <gabriximenes100@gmail.com>
2023-03-20 22:57:55 -0300

    update name

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         className="h-40 w-40 rounded-full" />
       <section className="mt-10 flex flex-col gap-2 text-center text-white">
         <div className="flex gap-2 items-center">
-          <h3 className="text-2xl font-semibold">Gabriele Ximenes </h3>
+          <h3 className="text-2xl font-semibold">Giselle Ximenes </h3>
           <img className="h-7 w-7" src="/verify.png" alt="" />
         </div>
         <span className="text-lg">Brasil</span>
