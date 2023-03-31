f965e3953455b1c46f7034b190658e36d6586169
ximeen <gabriximenes100@gmail.com>
2023-03-31 19:51:27 -0300

    link by whatts app

--- src/app/components/ProductLinks/index.tsx
+++ src/app/components/ProductLinks/index.tsx
@@ -7,7 +7,7 @@ export default function ProductLinks() {
     <section className="flex flex-col gap-4 pt-10 w-full md:justify-center md:items-center">
       <a
         target={"_blank"}
-        href="https://wa.me/5599991035812?text=Olá%20tenho%20interesse%20no%20LiftDetox"
+        href="https://wa.me/5599991436500?text=Olá%20tenho%20interesse%20no%20LiftDetox"
         className="flex p-2 bg-zinc-800 text-white rounded-md items-center justify-between md:w-1/2 lg:w-1/3 transition-all duration-150 hover:bg-zinc-700">
         <img
           className="w-12 h-12 rounded-md"
@@ -19,7 +19,7 @@ export default function ProductLinks() {
 
       <a
         target={"_blank"}
-        href="https://wa.me/5599991035812?text=Olá%20tenho%20interesse%20no%20Reveravit"
+        href="https://wa.me/5599991436500?text=Olá%20tenho%20interesse%20no%20Reveravit"
         className="flex p-2 bg-zinc-800 text-white rounded-md items-center justify-between md:w-1/2 lg:w-1/3 transition-all duration-150 hover:bg-zinc-700">
         <img
           className="w-12 h-12 rounded-md"
@@ -31,7 +31,7 @@ export default function ProductLinks() {
 
       <a
         target={"_blank"}
-        href="https://wa.me/5599991035812?text=Olá%20tenho%20interesse%20no%20Rosa%20Amazonica"
+        href="https://wa.me/5599991436500?text=Olá%20tenho%20interesse%20no%20Rosa%20Amazonica"
         className="flex p-2 bg-zinc-800 text-white rounded-md items-center justify-between md:w-1/2 lg:w-1/3 transition-all duration-150 hover:bg-zinc-700">
         <img
           className="w-12 h-12 rounded-md"
